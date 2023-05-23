35403e0c0518d351b73b91d395dac7698f4a05cb
kevinta893 <kevinta893@gmail.com>
2023-05-23 09:33:19 +0900

    💄 Add accordian to settings

---
+++ web-ui/src/lib/component/Collapse.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { ChevronDown } from 'svelte-heros-v2';
+	import { ChevronUp } from 'svelte-heros-v2';
+
+	export let title: string;
+
+	let isOpen: boolean = false;
+</script>
+
+<div class="collapse rounded-md" {...$$restProps}>
+	<input type="checkbox" bind:checked={isOpen} />
+	<div class="collapse-title flex justify-between items-center text-xl">
+		{title}
+		{#if isOpen}
+			<svelte:component this={ChevronDown} size={'24'} />
+		{:else}
+			<svelte:component this={ChevronUp} size={'24'} />
+		{/if}
+	</div>
+	<div class="collapse-content">
+		<slot />
+	</div>
+</div>
--- web-ui/src/lib/component/Webcam.svelte
+++ web-ui/src/lib/component/Webcam.svelte
@@ -12,6 +12,7 @@
 		setSaturation
 	} from '$lib/webcam/webcamControl';
 	import Button from './Button.svelte';
+	import Collapse from './Collapse.svelte';
 
 	export let webcamId: number;
 
@@ -47,24 +48,26 @@
 	</figure>
 	<div class="card-body">
 		<div class="card-actions justify-end">
-			Brightness
-			<Range min={-100} max={100} step={10} bind:value={brightness} />
-			Exposure
-			<Range min={-100} max={100} step={10} bind:value={exposure} />
-			Saturation
-			<Range min={-100} max={100} step={10} bind:value={saturation} />
-			Hue
-			<Range min={-100} max={100} step={10} bind:value={hue} />
-			<div class="flex w-full justify-between">
-				<Button on:click={resetSettings}>Reset</Button>
-				<Button primary on:click={togglePlay}>
-					{#if playing}
-						<Pause class="select-none" />
-					{:else}
-						<Play class="select-none" />
-					{/if}
-				</Button>
-			</div>
+			<Collapse title="Settings">
+				Brightness
+				<Range min={-100} max={100} step={10} bind:value={brightness} />
+				Exposure
+				<Range min={-100} max={100} step={10} bind:value={exposure} />
+				Saturation
+				<Range min={-100} max={100} step={10} bind:value={saturation} />
+				Hue
+				<Range min={-100} max={100} step={10} bind:value={hue} />
+				<div class="flex w-full justify-between">
+					<Button on:click={resetSettings}>Reset</Button>
+					<Button primary on:click={togglePlay}>
+						{#if playing}
+							<Pause class="select-none" />
+						{:else}
+							<Play class="select-none" />
+						{/if}
+					</Button>
+				</div>
+			</Collapse>
 		</div>
 	</div>
 </div>
